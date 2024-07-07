84c67a8ea24eb6df2a9888db930deda811fdc772
Pratiksha Datir@DESKTOP-E243SNP <pratikshadatir8@gmail.com>
2024-07-07 23:51:20 +0530

    stack

---
+++ Stack/src/stack/stack.java
@@ -0,0 +1,49 @@
+package stack;
+
+public class stack {
+    private int arr[];
+	private int top=-1;
+    private final int  SIZE;
+	
+    
+	
+   public stack(int size) {
+		super();
+		SIZE = size;
+		arr=new int [size];
+		top=-1;
+	}
+
+//	public stack( int size) {
+//		super();
+//	    arr = new int[size];
+//		this.top = -1;
+//		this.SIZE = size;
+//	}
+
+	
+	public void push(int data)
+	{
+		top++;
+		arr[top]=data;
+	}
+	
+	public int pop()
+	{
+		return arr[top--];
+		
+	}
+	public int peek()
+	{
+		return arr[top];
+	}
+	public boolean isEmpty()
+	{
+		return top==-1;
+	}
+	
+	public boolean isFull()
+	{
+		return top==(SIZE-1);
+	}
+}
